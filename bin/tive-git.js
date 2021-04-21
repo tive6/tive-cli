9ebbc46ef2d55127286f9e9655e027ae9ec0b4f3
zhouxianfu <zhouxianfu@yiche.com>
2021-04-21 14:26:14 +0800

    undefined

--- bin/tive-git.js
+++ bin/tive-git.js
@@ -7,16 +7,16 @@ const { push, merge } = require('../src/git-handler')
 const program = new Command()
 program
     .name('tive git')
-    // .usage('<项目名称> [tive-demo]')
+    .usage('<项目名称> [tive-demo]')
     .option('-b, --branch <type>', 'current branch', 'master')
     .option('-t, --target <type>', 'merged branch')
     .option('-m, --commit <type>', 'commit description')
     .option('-h, --help', 'view help information')
 program.parse();
 const options = program.opts()
-console.log(options)
+// console.log(options)
 const args = program.args
-console.log(args)
+// console.log(args)
 
 if (options && options.target) {
     merge(options)
